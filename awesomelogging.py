5fc9536f8ad63475f8a9e1f970286a54ebfe7a6b
Alex Lourie <djay.il@gmail.com>
2013-08-06 00:51:33 +0300

    initial work
    Signed-off-by: Alex Lourie <djay.il@gmail.com>

---
+++ awesomelogging.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import time
+import bintrees
+
+
+def event_stream(filename):
+    root = bintrees.AVLTree()
+    with open(filename, 'r') as log:
+        while True:
+            line = log.readline().split()
+            if len(line) > 0:
+                root.insert(line[0], ','.join(line[1:]))
+
+                delta = float(root.max_key()) - float(root.min_key())
+                if delta > 300:
+                    stamp, val = root.pop_min()
+                    yield '{stamp} {val}'.format(
+                        stamp=stamp,
+                        val=val,
+                    )
+            else:
+                time.sleep(2) 
+
+if __name__ == "__main__":
+    for event in event_stream('1'):
+        print event
